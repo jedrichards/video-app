e6afe7371f8c3459665fd5252b310c2db6a7a14f
jedrichards <jedrichards@gmail.com>
2022-10-10 13:44:01 +0100

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import styles from "./App.module.css";
 import buttonStyles from "./Button.module.css";
 import { Button } from "./Button";
 import { useKey } from "./useKey";
-// @ts-expect-error untyped module
-import videoCanvas from "video-canvas";
 import useResizeObserver from "@react-hook/resize-observer";
 import useEvent from "@react-hook/event";
 import interact from "interactjs";
@@ -58,7 +56,10 @@ function App() {
       ? estimatedFPS
       : null;
 
-  const frame = typeof fps === "number" ? Math.round(currentTime * fps) : null;
+  const frame =
+    typeof fps === "number"
+      ? Math.floor((video.current?.currentTime || 0) * fps)
+      : null;
 
   const coords: Coords = useMemo(() => {
     const scaleFactor = canvasRect.width / videoNativeSize[0];
@@ -101,23 +102,29 @@ function App() {
     ];
   });
 
-  async function nextFrame() {
-    const start = video.current?.currentTime || 0;
-    try {
-      // @ts-expect-error Firefox only API
-      await video.current?.seekToNextFrame();
-    } catch (e) {}
-    // Hack to update current time after seek
-    await video.current?.play();
-    await video.current?.pause();
-    // Rough estimate FPS
-    const end = video.current?.currentTime || 0;
-    setEstimatedFPS(1 / (end - start));
-  }
+  // async function nextFrame() {
+  //   const start = video.current?.currentTime || 0;
+  //   try {
+  //     // @ts-expect-error Firefox only API
+  //     await video.current?.seekToNextFrame();
+  //   } catch (e) {}
+  //   // Hack to update current time after seek
+  //   await video.current?.play();
+  //   await video.current?.pause();
+  //   // Rough estimate FPS
+  //   const end = video.current?.currentTime || 0;
+  //   setEstimatedFPS(1 / (end - start));
+  // }
 
-  async function nextFrameFPS() {
+  async function nextFrame() {
     if (userFPS === null) return;
     video.current!.currentTime = video.current!.currentTime + 1 / userFPS;
+    await video.current!.pause();
+  }
+
+  async function previousFrame() {
+    if (userFPS === null) return;
+    video.current!.currentTime = video.current!.currentTime - 1 / userFPS;
     await video.current!.pause();
   }
 
@@ -139,9 +146,25 @@ function App() {
     setEntries((prev) => ({ ...prev, [currentFrame]: coords }));
   }
 
-  useKey("KeyQ", () => nextFrame());
+  function updateCanvas() {
+    if (!canvas.current || !video.current) return;
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = video.current.videoWidth;
+    canvas.current.height = video.current.videoHeight;
+    ctx?.drawImage(
+      video.current,
+      0,
+      0,
+      canvas.current.width,
+      canvas.current.height
+    );
+    requestAnimationFrame(updateCanvas);
+  }
+
+  useKey("KeyQ", () => previousFrame());
+  useKey("KeyW", () => nextFrame());
+  useKey("KeyE", () => log());
   useKey("KeyF", () => promptFPS());
-  useKey("KeyW", () => log());
   useKey("Space", () => playPause());
 
   useResizeObserver(box.current, (entry) =>
@@ -177,9 +200,15 @@ function App() {
   useEffect(() => {
     if (!file) return;
 
-    videoCanvas(video.current, {
-      canvas: canvas.current,
-    });
+    promptFPS();
+  }, [file]);
+
+  useEffect(() => {
+    if (!file || userFPS === null) return;
+
+    // videoCanvas(video.current, {
+    //   canvas: canvas.current,
+    // });
 
     interact(box.current!)
       .draggable({
@@ -200,7 +229,9 @@ function App() {
           },
         },
       });
-  }, [file]);
+
+    requestAnimationFrame(updateCanvas);
+  }, [file, userFPS]);
 
   return (
     <div className={styles.app}>
@@ -244,6 +275,9 @@ function App() {
               onTimeUpdate={(e) => {
                 setCurrentTime(e.currentTarget.currentTime);
               }}
+              onFocus={(e) => {
+                e.currentTarget.blur();
+              }}
               onLoadedMetadata={(e) => {
                 setDuration(e.currentTarget.duration);
                 setVideoNativeSize([
@@ -253,10 +287,11 @@ function App() {
               }}
             />
             <div className={styles.buttonsAndInfo}>
-              <Button onClick={() => playPause()}>Play/Pause (space)</Button>
-              <Button onClick={() => nextFrame()}>Next frame (q)</Button>
-              <Button onClick={() => log()}>Log entry (w)</Button>
+              <Button onClick={() => previousFrame()}>Prev frame (q)</Button>
+              <Button onClick={() => nextFrame()}>Next frame (w)</Button>
+              <Button onClick={() => log()}>Log entry (e)</Button>
               <Button onClick={() => promptFPS()}>Set FPS (f)</Button>
+              <Button onClick={() => playPause()}>Play/Pause (space)</Button>
               <CSVLink
                 data={csvData}
                 className={buttonStyles.button}
@@ -279,7 +314,7 @@ function App() {
                 <div className={styles.info}>⚠️ FPS unknown</div>
               )}
               {typeof frame === "number" ? (
-                <div className={styles.info}>Frame {Math.round(frame)}</div>
+                <div className={styles.info}>Frame {frame}</div>
               ) : null}
               <div className={styles.info}>Box {printCoords(coords)}</div>
             </div>
